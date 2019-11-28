251adcb855bbae73b8870928069972012bd88be2
ExZero777 <Stratos@Stratos-PC>
2019-11-28 19:30:45 -0300

    Tp Maquina Cafe Finish

--- src/acme/Bebidas.java
+++ src/acme/Bebidas.java
@@ -1,19 +1,103 @@
 package acme;
 
+import java.util.Scanner;
+
 public abstract class Bebidas {
     protected String[] tip;
     protected int[] azu ;
     protected int[] lech;
     protected int con,men;
     protected float[] subT;
+    protected int[] opc;
  
+    Scanner sc = new Scanner(System.in);     
     
-    abstract void tipo();
-    abstract void azucar();
-    abstract void leche();
+    protected void tipo(){
+        if(men==1){ 
+            System.out.println("Desea Cafe Expresso(Opcion 1),Ristretto(Opcion 2),Normal(Opcion 3)"); 
+            men=sc.nextInt();
+            switch(men){
+                case 1: 
+                    tip[con]="Expresso";    
+                    subT[con]=subT[con]+100;
+                break;
+                case 2:
+                    tip[con]="Ristretto";    
+                    subT[con]=subT[con]+110;
+                break; 
+                case 3:
+                    tip[con]="Normal";    
+                    subT[con]=subT[con]+90;    
+                break;
+            } 
+           opc[con]=1;
+        }
+        else{
+            System.out.println("Desea Te Negro(Opcion 1),Rojo(Opcion 2),Verde(Opcion 3)"); 
+            men=sc.nextInt();
+            switch(men){
+                case 1:
+                    tip[con]="Negro";    
+                    subT[con]=subT[con]+70;    
+                break;   
+                case 2:
+                    tip[con]="Rojo";  
+                    subT[con]=subT[con]+70;
+                break;         
+                case 3:
+                    tip[con]="Verde";    
+                    subT[con]=subT[con]+75;
+                break;
+            }
+            opc[con]=2;
+        }
+        
+    }
     
-  
+    protected void azucar(){
+        System.out.println("Que Cantidad De Azucar Desea Agregar?(Ingrese Del 1 Al 3)");
+        azu[con]=sc.nextInt();
+        switch(azu[con]){
+            case 1:
+                subT[con]=subT[con]+5;
+                break;
+            case 2:
+                subT[con]=subT[con]+10;
+                break;
+            case 3:
+                subT[con]=subT[con]+15;
+                break;   
+        }
+    
+    }
+    
+    protected void leche(){
+        
+          System.out.println("Que Cantidad De Leche Desea Agregar?(Ingrese Del 1 Al 3)");
+        lech[con]=sc.nextInt();
+        switch(lech[con]){   
+            case 1:
+                subT[con]=subT[con]+5;
+                break;
+            case 2:
+                subT[con]=subT[con]+10;
+                break;
+            case 3:     
+                subT[con]=subT[con]+15;
+                break;
+        } 
+        
+        
+    }
 
+    public int[] getOpc() {
+        return opc;
+    }
+
+    public void setOpc(int[] opc) {
+        this.opc = opc;
+    }
+    
     public String[] getTip() {
         return tip;
     }
@@ -62,13 +146,15 @@ public abstract class Bebidas {
         this.subT = subT;
     }
 
-      Bebidas(String[] tiposs, int[] azucar, int[] leche, int cont, float[] subTotal,int menu) {
-        this.tip=tiposs;
+      Bebidas(String[] tipos, int[] azucar, int[] leche, int cont, float[] subTotal,int menu,int[] opcion) {
+        this.tip=tipos;
         this.azu=azucar;
         this.lech=leche;
         this.con=cont;   
         this.subT=subTotal;
         this.men=menu;
+        this.opc=opcion;
+        
     }
     
 
--- src/acme/Te.java
+++ src/acme/Te.java
@@ -1,67 +1,26 @@
 package acme;
 
-import java.util.Scanner;
-
 public class Te extends Bebidas{ 
-    Scanner sc = new Scanner(System.in);  
+    
    
-    Te(String[] tipos, int[] azucar, int[] leche, int cont, float[] subTotal,int menu) {
-        super(tipos,azucar,leche,cont,subTotal,menu);   
+    Te(String[] tipos, int[] azucar, int[] leche, int cont, float[] subTotal,int menu,int[] opcion) {
+        super(tipos,azucar,leche,cont,subTotal,menu,opcion);   
     }
 
        
-  @Override
-    public void tipo(){
-       System.out.println("Desea Te Negro(Opcion 1),Rojo(Opcion 2),Verde(Opcion 3)"); 
-       men=sc.nextInt();
-       switch(men){
-            case 1:
-                tip[con]="Negro";    
-                subT[con]=subT[con]+70;
-                break;   
-            case 2:
-                tip[con]="Rojo";  
-                subT[con]=subT[con]+70;
-                break;         
-            case 3:
-                tip[con]="Verde";    
-                subT[con]=subT[con]+75;
-                break;
-        }
+   @Override
+    protected void tipo(){
+        super.tipo();
     }
 
     @Override
-    void azucar() {
-        System.out.println("Que Cantidad De Azucar Desea Agregar?(Ingrese Del 1 Al 3)");
-        azu[con]=sc.nextInt();
-        switch(azu[con]){
-            case 1:
-                subT[con]=subT[con]+5;
-                break;
-            case 2:
-                subT[con]=subT[con]+10;
-                break;
-            case 3:
-                subT[con]=subT[con]+15;
-                break;        
-        }
+    protected void azucar() {
+        super.azucar();
     }  
 
     @Override
-    void leche() {
-        System.out.println("Que Cantidad De Leche Desea Agregar?(Ingrese Del 1 Al 3)");
-        lech[con]=sc.nextInt();
-         switch(lech[con]){   
-            case 1:
-                subT[con]=subT[con]+5;
-                break;
-            case 2:
-                subT[con]=subT[con]+10;
-                break;
-            case 3:     
-                subT[con]=subT[con]+15;
-                break;
-        } 
+    protected void leche() {
+        super.leche();
     }  
     
 }
