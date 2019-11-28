97906717e68669b8ac608cf10933d9c1db64066f
ExZero777 <Stratos@Stratos-PC>
2019-11-28 19:29:43 -0300

    Tp Maquina Cafe Finish

--- src/acme/Acme.java
+++ src/acme/Acme.java
@@ -1,7 +1,7 @@
 package acme;
 
 import java.util.Scanner;
-
+ 
 public class Acme {
     
     public static void main(String[] args) {
@@ -12,25 +12,31 @@ public class Acme {
         int [] leche = new int[5];
         float [] subTotal =new float[5];
         int menu=0;
+        int [] opcion = new int[5];
         Scanner sc = new Scanner(System.in);
        
         while((n=='s') || (n=='S')){     
-            switch(ingreso(menu)){
+            menu = ingreso(menu);
+            switch(menu){
                 case 1:                   
-                    Bebidas ca = new Cafe(tipos,azucar,leche,cont,subTotal,menu);
+                    Bebidas ca = new Cafe(tipos,azucar,leche,cont,subTotal,menu,opcion);
                     ca.tipo();
                     ca.azucar();
                     ca.leche();
                     cont = cont+1;
                 break;          
                 case 2:
-                    Bebidas te = new Te(tipos,azucar,leche,cont,subTotal,menu);
+                    Bebidas te = new Te(tipos,azucar,leche,cont,subTotal,menu,opcion);
                     te.tipo();
                     te.azucar();
                     te.leche();
                     cont = cont+1;
                 break;              
-            
+                
+                case 3:
+                    System.exit(0);
+                break;    
+                
                 default:
                     System.out.println("O_o...,Eran 2 Opciones 1 O 2,Yo Que Usted Vuelvo A Recursar La Primaria...");
                     System.out.println(" ");
@@ -42,7 +48,7 @@ public class Acme {
             System.out.println("Desea Seguir Pagando Sabiendo Que No Le Voy A Dar Nada? S/N");
             n  = sc.next().charAt(0);
         }
-        totales(tipos, azucar, leche, subTotal);
+        totales(tipos,azucar,leche,subTotal,opcion);
     }
     
     public static int ingreso(int men){
@@ -51,17 +57,22 @@ public class Acme {
             System.out.println(" ");
             System.out.println("Desea Un Cafè(Ingrese 1) O Un Tè(Ingrese 2)?");
             System.out.println(" ");
-            System.out.println("O Tambien Puede Ingresar 3 Y Salir Del Sistema Y Permitirme No Verle Mas Lo Feo De Cara Que Es Y De Paso Seguir Llenandome De Guita Los Bolsillos");
+            System.out.println("O Tambien Puede Ingresar 3 Y Salir Del Sistemarijillo");
             System.out.println(" ");
-            int menu = sc.nextInt();
-        return menu;
+            int me = sc.nextInt();
+        return me;
     }
     
-    public static void totales(String[] tipos,int[] azucar,int[] leche,float[] subTotal){
+    public static void totales(String[] tipos,int[] azucar,int[] leche,float[] subTotal,int[] opcion){
         System.out.println("Aqui Esta El Resultarijillo");
         for(int i=0;i<tipos.length;i++){
             if(subTotal[i]!=0){
-                System.out.print("Cafè "+tipos[i]+" Con ");
+                if(opcion[i]==1){
+                    System.out.print("Cafè "+tipos[i]+" Con ");
+                }
+                else{
+                    System.out.print("Té "+tipos[i]+" Con ");
+                }
                 System.out.print(azucar[i]+" De Azucar Y ");
                 System.out.print(leche[i]+" De Leche: ");
                 System.out.print(subTotal[i]+"$ ");
@@ -71,7 +82,7 @@ public class Acme {
                 System.out.println("TOTAL: "+total+"$ ");
             }
         }
-        System.out.println("E e e Eso Es Todo Amigos..(Are Bugs Bunny) ");       
+        System.out.println("E e e Eso Es Todo Amigos..(Are Bugs Bunny) ");  
     }
         
 }
